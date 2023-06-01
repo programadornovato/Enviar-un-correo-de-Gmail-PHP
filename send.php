b132d13f785e5c9f03e75e1d5f5dd28f43911481
programadornovato <info@programadornovato.com>
2023-06-01 01:31:11 -0600

    quitar espacio

--- send.php
+++ send.php
@@ -1,12 +1,9 @@
 <?php
-
 //librerias
 require 'PHPMailer/PHPMailerAutoload.php';
-
 //Create a new PHPMailer instance
 $mail = new PHPMailer();
 $mail->IsSMTP();
-
 //Configuracion servidor mail
 $mail->From = "eucm2g@gmail.com"; //remitente
 $mail->SMTPAuth = true;
@@ -15,12 +12,10 @@ $mail->Host = "smtp.gmail.com"; // servidor smtp
 $mail->Port = 587; //puerto
 $mail->Username = 'eucm2g@gmail.com'; //nombre usuario
 $mail->Password = 'dksrptjthcdsydyd'; //contraseña
-
 //Agregar destinatario
 $mail->AddAddress($_POST['email']);
 $mail->Subject = $_POST['subject'];
 $mail->Body = $_POST['message'];
-
 //Avisar si fue enviado o no y dirigir al index
 if ($mail->Send()) {
   echo '<script type="text/javascript">
